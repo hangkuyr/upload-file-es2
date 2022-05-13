049d7719c9f6562aa36e34ea93858923f625d1b1
davi-v <davibtv@gmail.com>
2022-05-13 20:53:03 -0300

    tirando dependências não usadas para tentar passar no github actions

--- Project/src/pch.py
+++ Project/src/pch.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, render_template, send_file, send_from_directory, Response, request, redirect, url_for, flash
-from datetime import datetime, timedelta
-import pandas as pd, os, sqlite3, numpy as np, time, werkzeug, os, json, io
 
 # constants
 dir = os.path.dirname(__file__)
