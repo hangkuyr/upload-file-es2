d85b5f2a2fe6c73809038069f0af70594c2b61a7
davi-v <davibtv@gmail.com>
2022-05-13 21:02:56 -0300

    testando o teste quando o arquivo não existe ainda

--- Project/tests/test_SaveFile.py
+++ Project/tests/test_SaveFile.py
@@ -2,7 +2,7 @@ from includes import*
 from saveFile import*
 
 def test_SaveFile():
-	id = 'abc'
+	id = 'testFile'
 	content = b'hello'
 	SaveFile(id, content)
 	with open(PRIV_DIR + id, 'rb') as f:
