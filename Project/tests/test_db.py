2b99136cf320af55f03934e2f03d0b0efb6c7f2b
hangkuyr <laisglima@hotmail.com>
2022-05-29 21:58:36 -0300

    Teste save 2 files in DB

--- Project/tests/test_db.py
+++ Project/tests/test_db.py
@@ -6,7 +6,7 @@ class TestDB:
 	def setup_method(self):
 		self.db = DB()
 
-	def test_dbSaveFile(self):
+	def test_dbSaveFile_one(self):
 		id = 'id teste'
 		filename = 'teste.txt'
 		title = 'título teste'
@@ -16,6 +16,20 @@ class TestDB:
 		self.db._saveFileImpl(id, filename, title, desc, data, password)
 		dbItem = self.db.tryReadContents(id)
 		assert dbItem.title == title
+
+	def test_dbSaveFile_two(self):
+		id = 'id teste'
+		filename = 'teste.txt'
+		title = 'título teste'
+		data = b'teste bytes'
+		self.db._saveFileImpl(id, filename, title, '', data, '')
+		id2 = 'id teste 2'
+		filename2 = 'teste2.txt'
+		title2 = 'título teste 2'
+		data2 = b'teste 2 bytes'
+		self.db._saveFileImpl(id2, filename2, title2, '', data2, '')
+		assert self.db.size() == 2
+		
 
 	def test_dbSorted(self):
 		self.db._saveFileImpl('i1', 'f1', 't1', 'd1', b'b1', '')
