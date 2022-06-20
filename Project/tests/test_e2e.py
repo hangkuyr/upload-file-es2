fb7e1ce16c3469caef79ec2752b1dd573cc20987
hangkuyr <laisglima@hotmail.com>
2022-06-20 19:17:17 -0300

    Fix Selenium CI/CD Part 6

--- Project/tests/test_e2e.py
+++ Project/tests/test_e2e.py
@@ -3,14 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
-from pyvirtualdisplay import Display
 
     
 
 def test_first_case_selenium():
-
-	display = Display(visible=0, size=(800, 800))  
-	display.start()
 
 	chromedriver_autoinstaller.install()
 
@@ -23,7 +19,7 @@ def test_first_case_selenium():
 	 
 	    "--headless",
 	    #"--disable-gpu",
-	    #"--window-size=1920,1200",
+	    "--window-size=1920,1200",
 	    #"--ignore-certificate-errors",
 	    #"--disable-extensions",
 	    "--no-sandbox",
@@ -37,5 +33,6 @@ def test_first_case_selenium():
     
 	driver = webdriver.Chrome(options = chrome_options)
 	
-	driver.get("localhost:80")
+	#driver.get("localhost:8000")
+	driver.get("http://127.0.0.1")
 	assert "Upload File" in driver.title
